c25fbeffa4dc785afe61c883fc620725e43d7845
HushLuxe <maryukjane@gmail.com>
2026-02-03 17:20:30 +0100

    chore: update gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,20 @@ import Dashboard from './pages/Dashboard';
 import Settings from './pages/Settings';
 import ScanDetail from './pages/ScanDetail';
 import BrandPreview from './pages/BrandPreview';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
     <ToastProvider>
       <Routes>
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
         <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/settings" element={<Settings />} />
